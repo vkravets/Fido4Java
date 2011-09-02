4090fc81509e2e533ea2465594fbaad7d5f09e14
Vladimir Kravets <vova.kravets@gmail.com>
2011-09-02 17:42:40 +0300

    Add fake test for tosser

---
+++ jtoss/src/test/java/org/fidonet/tests/toss/TestTosser.java
@@ -0,0 +1,11 @@
+package org.fidonet.tests.toss;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: sly
+ * Date: 9/2/11
+ * Time: 5:42 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestTosser {
+}
