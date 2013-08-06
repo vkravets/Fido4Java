9aed2f6c9dd649b186ab50cbd55057c57d6a9f07
Vladimir Kravets <vova.kravets@gmail.com>
2013-08-06 15:55:31 +0300

    - Add scheduler plugin - Optimize maven dependencies

---
+++ scheduler/src/main/java/org/fidonet/jftn/scheduler/Scheduler.java
@@ -0,0 +1,50 @@
+package org.fidonet.jftn.scheduler;
+
+import it.sauronsoftware.cron4j.Task;
+import it.sauronsoftware.cron4j.TaskExecutionContext;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Author: Vladimir Kravets
+ * E-Mail: vladimir.kravets-ukr@hp.com
+ * Date: 8/6/13
+ * Time: 2:48 PM
+ */
+public class Scheduler {
+    
+    private it.sauronsoftware.cron4j.Scheduler scheduler;
+
+    public Scheduler() {
+        scheduler = new it.sauronsoftware.cron4j.Scheduler();
+    }
+    
+    public void start() {
+        scheduler.start();
+    }
+    
+    public void stop() {
+        scheduler.stop();
+    }
+    
+    public String schedule(String cronSchedule, Runnable task) {
+        return scheduler.schedule(cronSchedule, task);
+    }
+    
+    public void deschedule(String taskId) {
+        scheduler.deschedule(taskId);
+    }
+    
+    public void reschedule(String taskId, String cronTemplate) {
+        scheduler.reschedule(taskId, cronTemplate);
+    }
+    
+    public void launch(final Runnable runnable) {
+        Task task = new Task() {
+            @Override
+            public void execute(TaskExecutionContext context) throws RuntimeException {
+                runnable.run();
+            }
+        };
+        scheduler.launch(task);
+    }
+}
---
+++ scheduler/src/main/java/org/fidonet/jftn/scheduler/plugin/SchedulerPlugin.java
@@ -0,0 +1,43 @@
+package org.fidonet.jftn.scheduler.plugin;
+
+import org.fidonet.events.EventBus;
+import org.fidonet.jftn.plugins.Plugin;
+import org.fidonet.jftn.plugins.PluginException;
+import org.fidonet.jftn.plugins.PluginInformation;
+import org.fidonet.jftn.plugins.PluginManager;
+import org.fidonet.jftn.scheduler.Scheduler;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Author: Vladimir Kravets
+ * E-Mail: vladimir.kravets-ukr@hp.com
+ * Date: 8/6/13
+ * Time: 2:46 PM
+ */
+public class SchedulerPlugin implements Plugin {
+    
+    private Scheduler scheduler;
+    
+    @Override
+    public PluginInformation getPluginInfo() {
+        return new PluginInformation("schedule", 1, 0, "Cron-like scheduler functionality plugin");
+    }
+
+    @Override
+    public void init(PluginManager manager, EventBus eventBus) { }
+
+    @Override
+    public void load() throws PluginException {
+        scheduler = new Scheduler();
+    }
+
+    @Override
+    public void unload() throws PluginException {
+        scheduler = null;
+    }
+
+    @Override
+    public Object getContext() {
+        return scheduler;
+    }
+}
